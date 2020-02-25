2e0c030d23a84aedb94eac539b54bddf20d4d4b3
Adrian Cornish <github@bluedreamer.com>
2020-02-25 15:50:04 -0700

    Corner case parsing tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -17,3 +17,43 @@ TEST_CASE("Test parsing of first entry", "[parser]")
    REQUIRE(message->GetVolume() == 49);
    REQUIRE(message->GetPrice() == 72);
 }
+
+TEST_CASE("Test bad_trade1.csv", "[parser]")
+{
+   std::ifstream in("test_data/bad_trade1.csv");
+   CSVParser parser(in);
+
+   auto message = parser.GetMessage();
+}
+
+TEST_CASE("Test bad_trade2.csv", "[parser]")
+{
+   std::ifstream in("test_data/bad_trade2.csv");
+   CSVParser parser(in);
+
+   auto message = parser.GetMessage();
+}
+
+TEST_CASE("Test bad_trade3.csv", "[parser]")
+{
+   std::ifstream in("test_data/bad_trade3.csv");
+   CSVParser parser(in);
+
+   auto message = parser.GetMessage();
+}
+
+TEST_CASE("Test bad_trade4.csv", "[parser]")
+{
+   std::ifstream in("test_data/bad_trade4.csv");
+   CSVParser parser(in);
+
+   auto message = parser.GetMessage();
+}
+
+TEST_CASE("Test bad_trade5.csv", "[parser]")
+{
+   std::ifstream in("test_data/bad_trade5.csv");
+   CSVParser parser(in);
+
+   auto message = parser.GetMessage();
+}
