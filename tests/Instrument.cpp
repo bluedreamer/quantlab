92fc82b1d7ef76c0d971461aaf70f9dd0ce62491
Adrian Cornish <github@bluedreamer.com>
2020-02-25 17:48:23 -0700

    Added correct values to Instrumen tests

--- tests/Instrument.cpp
+++ tests/Instrument.cpp
@@ -6,9 +6,9 @@ TEST_CASE("Default ctor on instrument", "[instrument]")
 {
    Instrument instrument;
 
-   REQUIRE(instrument.GetTotalVolume() == 234);
-   REQUIRE(instrument.GetLargestTradeGap() == 234);
-   REQUIRE(instrument.GetAverageWeightedPrice() == Approx(3.14));
+   REQUIRE(instrument.GetTotalVolume() == 0);
+   REQUIRE(instrument.GetLargestTradeGap() == 0);
+   REQUIRE(instrument.GetAverageWeightedPrice() == Approx(0.0));
 }
 
 TEST_CASE("Single trade on instrument", "[instrument]")
@@ -16,9 +16,10 @@ TEST_CASE("Single trade on instrument", "[instrument]")
    Instrument instrument;
 
    instrument.Trade(51300130197, 286, 202);
-   REQUIRE(instrument.GetTotalVolume() == 234);
-   REQUIRE(instrument.GetLargestTradeGap() == 234);
-   REQUIRE(instrument.GetAverageWeightedPrice() == Approx(3.14));
+
+   REQUIRE(instrument.GetTotalVolume() == 286);
+   REQUIRE(instrument.GetLargestTradeGap() == 0);
+   REQUIRE(instrument.GetAverageWeightedPrice() == Approx(202.0));
 }
 
 TEST_CASE("Multiple trades on instrument", "[instrument]")
@@ -28,7 +29,8 @@ TEST_CASE("Multiple trades on instrument", "[instrument]")
    instrument.Trade(51300130197, 286, 202);
    instrument.Trade(51304771475, 78, 199);
    instrument.Trade(51311533907, 19, 185);
-   REQUIRE(instrument.GetTotalVolume() == 234);
-   REQUIRE(instrument.GetLargestTradeGap() == 234);
-   REQUIRE(instrument.GetAverageWeightedPrice() == Approx(3.14));
+
+   REQUIRE(instrument.GetTotalVolume() == 383);
+   REQUIRE(instrument.GetLargestTradeGap() == 11'403'710);
+   REQUIRE(instrument.GetAverageWeightedPrice() == Approx(200.545691906005));
 }
