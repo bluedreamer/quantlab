d8f5514f3651a2f5995a36a5fd88b34bfd27e60c
Adrian Cornish <github@bluedreamer.com>
2020-02-25 17:41:27 -0700

    More Instrument tests

--- tests/Instrument.cpp
+++ tests/Instrument.cpp
@@ -11,3 +11,24 @@ TEST_CASE("Default ctor on instrument", "[instrument]")
    REQUIRE(instrument.GetAverageWeightedPrice() == Approx(3.14));
 }
 
+TEST_CASE("Single trade on instrument", "[instrument]")
+{
+   Instrument instrument;
+
+   instrument.Trade(51300130197, 286, 202);
+   REQUIRE(instrument.GetTotalVolume() == 234);
+   REQUIRE(instrument.GetLargestTradeGap() == 234);
+   REQUIRE(instrument.GetAverageWeightedPrice() == Approx(3.14));
+}
+
+TEST_CASE("Multiple trades on instrument", "[instrument]")
+{
+   Instrument instrument;
+
+   instrument.Trade(51300130197, 286, 202);
+   instrument.Trade(51304771475, 78, 199);
+   instrument.Trade(51311533907, 19, 185);
+   REQUIRE(instrument.GetTotalVolume() == 234);
+   REQUIRE(instrument.GetLargestTradeGap() == 234);
+   REQUIRE(instrument.GetAverageWeightedPrice() == Approx(3.14));
+}
